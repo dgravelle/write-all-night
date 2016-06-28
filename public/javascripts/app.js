a9737db1132dd5749abfa302de596b6ed4af5e8a
Dave Gravelle <dgravelle54@gmail.com>
2016-06-27 20:41:32 -0700

    user dashboard behind login wall

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,12 +2,30 @@
 
   angular
     .module('app', ['ngRoute'])
-    .config(function($routeProvider) {
+    .config(function($routeProvider, $httpProvider) {
       $routeProvider
         .when('/', {
           templateUrl: 'views/register.html',
           controller: 'RegisterController'
         })
+        .when('/users/:id', {
+          templateUrl: 'views/dashboard.html',
+          restricted: true,
+          resolve: {
+            getUser: function (UserService, $route) {
+              return UserService.getUser($route.current.params.id);
+            }
+          }
+        });
+        $httpProvider.interceptors.push('AuthInterceptor');
+    })
+
+    .run(function($rootScope, $location, $window, UserService) {
+      $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        if (next.restricted && !$window.localStorage.getItem('user')) {
+          $location.path('/');
+        }
+      })
     })
 
 })()
