6159cdb1bf8d590a3a33971735bbe39a9d0973e7
Dave Gravelle <dgravelle54@gmail.com>
2016-07-05 17:43:30 -0700

    finished request for calendar query

--- routes/stories.js
+++ routes/stories.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
 const knex = require('../db/knex');
 const Stories = knex('stories');
 const StoryProgress = knex('story_progress');
@@ -61,16 +62,19 @@ group by day order by day;
 
 */
 
-select date_saved, word_total, story_id, user_id 
-from story_progress t1
-where date_saved = (
-  select max(date_saved)
-  from story_progress t2
-  where t1.story_id = t2.story_id
-)
-group by story_id;
+// select date_saved, word_total, story_id, user_id
+// from story_progress t1
+// where date_saved = (
+//   select max(date_saved)
+//   from story_progress t2
+//   where t1.story_id = t2.story_id
+// )
+// group by story_id;
 
+var firstDate = moment([2016, 7 - 1])
+var lastDate = moment().endOf('month');
 
+console.log(firstDate.format());
 
 router.post('/', (req, res) => {
   console.log(req.body);
@@ -81,6 +85,40 @@ router.post('/', (req, res) => {
   .catch(err => {
     console.log(err);
 
+    res.json(err);
+  })
+});
+
+router.get('/calendar/', (req, res) => {
+
+  StoryProgress.select().where('date_saved', '>=', firstDate.format()).orderBy('date_saved', 'asc')
+  .then(data => {
+
+    var results = [];
+    var lastEntry;
+    var currentDay = moment(data[0]).get('date');
+
+    for (var i = 0; i < data.length; i++) {
+
+      if (currentDay !== moment(data[i].date_saved).get('date')) {
+        currentDay = moment(data[i].date_saved).get('date');
+        lastEntry = null;
+      }
+      else {
+        if (!lastEntry) {
+          lastEntry = data[i];
+          results.push(lastEntry);
+        }
+        else if (lastEntry.date_saved < data[i].date_saved) {
+          results.splice(results.indexOf(lastEntry), 1, data[i]);
+          lastEntry = data[i];
+        }
+      }
+    }
+
+    res.json(results);
+  })
+  .catch(err => {
     res.json(err);
   })
 });
@@ -113,6 +151,7 @@ router.get('/all/:id', (req, res) => {
     res.json(err);
   })
 });
+
 
 router.get('/latest/:id', (req, res) => {
   const user_id = req.params.id;
--- seeds/story_progress.js
+++ seeds/story_progress.js
@@ -1,28 +1,45 @@
 var moment = require('moment');
 
 
-// console.log(story1promise);
+// console.log(stories);
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('story_progress').del()
     .then(function () {
 
-      var startDate = moment('2016-07-01 13:00:00');
-      var story1promise = [];
-      var date_saved = startDate;
+      var stories = [];
+      var startDates = [
+        moment('2016-06-30 13:00:00'),
+        moment('2016-07-01 08:00:00'),
+        moment('2016-07-02 08:00:00'),
+        moment('2016-07-03 08:00:00'),
+        moment('2016-07-05 10:00:00'),
+        moment('2016-07-06 10:00:00'),
+        moment('2016-07-13 10:00:00'),
+        moment('2016-07-14 10:00:00'),
+        moment('2016-07-15 10:00:00')
+      ]
+
       var word_total = 0;
 
-      for (var i = 0; i < 60; i++) {
+      startDates.map(date => {
+        // var i = Math.ceil(Math.random() * 10) * 5;
+        makeStory(date, 50);
+      })
 
-        story1promise.push(knex('story_progress').insert({
-          user_id: 1,
-          story_id: 1,
-          word_total: word_total += 50,
-          date_saved: startDate.add(1, 'm').format()
-        }));
+      function makeStory(date, wordIncrement) {
+        word_total = 0;
+        for (var i = 0; i < 60; i++) {
+          stories.push(knex('story_progress').insert({
+            user_id: 1,
+            story_id: 1,
+            word_total: word_total += wordIncrement,
+            date_saved: date.add(1, 'm').format()
+          }));
+        }
       }
-      console.log();
-      return Promise.all(story1promise);
+
+      return Promise.all(stories);
     });
 };
