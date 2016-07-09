d62e488a8cda9d0b78815cc844463c625a893c93
Dave Gravelle <dgravelle54@gmail.com>
2016-07-09 16:42:05 -0700

    initialized calendar with current month and year

--- public/javascripts/controllers/dashboard.controller.js
+++ public/javascripts/controllers/dashboard.controller.js
@@ -8,78 +8,15 @@
 
     function DashboardController($scope, $timeout, UserService, getUser, allStories, writingProgress) {
 
+      // story chart
       $scope.storyData = writingProgress;
       $scope.currentUser = getUser;
       $scope.writingProgress = writingProgress;
       $scope.allStories = allStories;
 
-      // console.log(writingProgress);
-
-      // function getLatestStory() {
-      //   debugger;
-      //   var latest = writingProgress[writingProgress.length - 1];
-      //
-      //   for (var i = 0; i < $scope.allStories.length; i++) {
-      //     if ($scope.allStories[i].id === latest.story_id) {
-      //       $scope.latestStory = $scope.allStories[i];
-      //       $scope.latestStory.word_total = latest.word_total;
-      //       console.log($scope.latestStory);
-      //     }
-      //   }
-      // }
-      //
-      // var chartMonth = [];
-      // var chartPoints = [];
-      //
-      // for (var i = 1; i <= moment().daysInMonth(); i++) {
-      //   chartMonth.push(i.toString());
-      // }
-
-      // function createPoints(progress) {
-      //
-      //   var obj = {};
-      //   for (var i = 0; i < $scope.writingProgress.length; i++) {
-      //     var x = moment($scope.writingProgress[i].date_saved).get('date');
-      //     var y = $scope.writingProgress[i].word_total;
-      //     obj[x] = y;
-      //   }
-      //   // return obj;
-      //   var dataPointsz = [];
-      //   var lastTotal = 0;
-      //
-      //   for (var i = 1; i <= moment().get('date'); i++) {
-      //     if (obj.hasOwnProperty(i)) {
-      //       lastTotal = obj[i];
-      //       dataPointsz.push(lastTotal);
-      //     }
-      //     else {
-      //       dataPointsz.push(lastTotal);
-      //     }
-      //   }
-      //   return dataPointsz;
-      // }
-
-      // getLatestStory()
-      // $timeout(, 0)
-
-      // $scope.chartData = createPoints($scope.writingProgress);
-      // $scope.labels = chartMonth;
-      // $scope.points = [$scope.chartData];
-
-      //
-      // $scope.option = {
-      //   scaleOverride: true,
-      //   scaleSteps: steps,
-      //   scaleStepWidth: Math.ceil(50000 / steps),
-      //   scaleStartValue: 0
-      // }
-
-      // console.log($scope.chartData);
-
-
-
-      // $timeout(() => {
-      // },0)
+      // writing progress
+      // $scope.month = moment().month();
+      // $scope.year = moment().year();
 
     }
 })()
