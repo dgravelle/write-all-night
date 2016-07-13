d66fbaeb57876c325b6845c92fef8aec47171162
Dave Gravelle <dgravelle54@gmail.com>
2016-07-13 16:03:30 -0700

    consolidated story progress report into one api request

--- routes/progress.js
+++ routes/progress.js
@@ -12,11 +12,29 @@ function StoryProgress() {
   return knex('story_progress');
 }
 
-router.get('/progress/:storyId', (req, res) => {
-  const id = req.params.storyId;
+router.get('/:userId', (req, res) => {
+  const id = req.params.userId;
 
-  StoryProgress().select().where('story_id', storyId).then(data => {
-    console.log(data);
+  StoryProgress().select().where('user_id', id).first().then(data => {
+    // console.log(data);
+
+    Stories().select().where({ id: data.story_id }).first().then(data => {
+      // console.log('story: ', data);
+
+      var storyInfo = data;
+
+      StoryProgress().select().where({ story_id: data.id, user_id: data.user_id }).then(progress => {
+
+        var storyProgress = progress;
+        var storyReport = {
+          info: storyInfo,
+          progress: progress
+        }
+
+        res.json(storyReport);
+      })
+
+    })
   })
   .catch(err => {
     console.log(err);
